2e5ba8309736d2189661114a55bee5d2e428fc92
Hsiao <chiyen29@gmail.com>
2013-11-05 17:46:44 +0800

    XX
    XX

---
+++ include_login_chiyen.php
@@ -0,0 +1,9 @@
+<div class="login">
+                	<div class="forgot-pw"><a href="#" class="font-purple">忘記密碼</a></div>
+                    <form>
+                      <input name="" type="text" placeholder="帳號">
+                      <input name="" type="text" placeholder="密碼">
+                    </form>
+                    <button id="signin">登入</button>
+                    <button id="register">加入會員</button>
+                </div>
