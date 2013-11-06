2d70e9c92caa8230a587dfa0dc2569b0b4c0256b
Tina <to.tinalai@gmail.com>
2013-11-07 00:42:31 +0800

    Revert "團體聯誼 列表/內容頁"
    This reverts commit 96a0b0414c36e20a21adca9906e0a8d8f6c9c70c.

--- include_scripts.php
+++ include_scripts.php
@@ -8,7 +8,6 @@
 			// setup ul.tabs to work as tabs for each div directly under div.panes
 			$("#index_mem ul.tabs").tabs(" div.p-mem > div");
 			$("#index_area ul.tabs").tabs(" div.p-area > div");
-			$("#group_info ul.tabs").tabs(" div.p-info > div");
 		});
 		
 </script>
