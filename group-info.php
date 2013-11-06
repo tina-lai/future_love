80f7e55e8aead31e69e80465a4471cafebd97fdd
Tina <to.tinalai@gmail.com>
2013-11-07 00:43:48 +0800

    Revert "Revert "團體聯誼 列表/內容頁""
    This reverts commit 2d70e9c92caa8230a587dfa0dc2569b0b4c0256b.

---
+++ group-info.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+    <title>戀戀未來</title>
+
+    
+    <link href="css/normalize.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/layout.css" rel="stylesheet">
+    <link href="css/tabs.css" rel="stylesheet">
+
+    <!-- HTML5 shim and IE8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="/js/html5shiv.js"></script>
+    <![endif]-->
+    
+   
+  </head>
+
+<body>
+    
+<!-- include header -->	
+ <?php include("include_header.php");?>
+ 
+	
+    <section class="wrapper clearfix">
+    	
+        <div class="since"><img src="images/since.png"></div>
+        
+        <!-- 右方搜尋-->
+		 <?php include("include_right_search.php");?>
+        <!-- end 右方搜尋-->
+        
+        <div id="content" class="clearfix">
+        
+        	<div class="banner" style="width:940px; height:300px;">
+       	    	<img src="images/banner.jpg"> 
+            </div>
+        	
+            
+            <!-- 左邊欄位-->
+            <div id="left-col">
+                
+                <!-- 左欄內容-->
+               	<?php include("include_left-col.php");?>
+                <!-- end 左欄內容-->
+                
+          </div><!-- end  左邊欄位-->
+            
+            <div id="right-col">
+            	<div class="text-slider">
+           	    	<img src="images/icon-logo-purple.png" width="18" height="16"> 
+                    <span class="font-red">10/05  新竹地區 戶外聯誼   </span>
+                    <span class="font-blue">☆ 企業聯誼☆  戀戀香草幸福出遊-戶外之旅，目前男性報名資料.   >小帆25 歲 / 桃園縣 </span>  
+                </div>
+                
+                <h2 class="heading-purple">
+                	台北聯誼| 台北未婚聯誼| 台北單身聯誼活動-2013/12/14
+                </h2>
+                <div class="heading-dashline"></div>
+                
+                <div class="meeting-info clearfix">
+                	<div class="image"><img src="images/meeting.jpg" width="321" height="239"></div>
+                    <div class="details">
+                    	<table class="table-details" width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td width="100">‧ 聯誼日期：</td>
+                            <td class="color-orange">2013/12/14(六)</td>
+                          </tr>
+                          <tr>
+                            <td>‧ 時        間：</td>
+                            <td class="color-orange">12:00~15:30</td>
+                          </tr>
+                          <tr>
+                            <td>‧ 報到時間：</td>
+                            <td class="color-orange">2013/12/14(六)</td>
+                          </tr>
+                          <tr>
+                            <td>‧ 男女人數：</td>
+                            <td class="color-orange">12 vs 12 ( 預計 ) - 後補各 1位</td>
+                          </tr>
+                          <tr>
+                            <td>‧ 活動地點：</td>
+                            <td class="color-orange">書香怡園 ( 原：書香名門 ) <a href="#">店家資訊</a></td>
+                          </tr>
+                          <tr>
+                            <td>‧ 活動地址：</td>
+                            <td class="color-orange">北市中正區博愛路9號2樓-捷運西門站5號
+                        出口步行約10分鐘</td>
+                          </tr>
+                          <tr>
+                          	<td></td>
+                            <td>
+                            	<button class="btn-purple">地圖</button>
+                            	<button class="btn-purple">我要報名</button>
+                            </td>
+                          </tr>
+                        </table>
+
+                    </div>
+                </div>
+                
+                <div class="page-content">
+                	<h3>>> 活動特色</h3>
+                    
+                  <h2 class="heading-pink-66">
+                  <img src="images/icon-heart.png" width="15" height="14">專屬一對一，約會我和你(妳)  <img src="images/icon-heart.png" width="15" height="14"></h2>
+                  <div class="dashline"></div>
+                    
+                    <table class="table01" width="100%" cellspacing="2" cellpadding="2">
+                      <tr>
+                        <td width="150" class="bg-purple text-right">約會時間：</td>
+                        <td class="bg-blue">2013.12.14(六)  12:00～15:30</td>
+                      </tr>
+                      <tr>
+                        <td class="bg-purple text-right">幸福報到：</td>
+                        <td class="bg-blue">11:30</td>
+                      </tr>
+                      <tr>
+                        <td class="bg-purple text-right">預計人數：</td>
+                        <td class="bg-blue">男仕12 vs 女仕12-後補男女各1位--婚姻限定首次--</td>
+                      </tr>
+                      <tr>
+                        <td class="bg-purple text-right">幸福邀約：</td>
+                        <td class="bg-blue"><span class="font-bright-pink">★淑女:大學畢、優雅清秀、內外兼具、公教國營優質女仕  (另有同行優惠-詳見特
+   別優惠)</span><br>
+<span class="font-blue">★紳士:各界社會菁英，國營公教、碩博菁英、科技新貴、自營商、百大
+   上市櫃公司或年收入達80萬 以上之優質男仕尤佳</span></td>
+                      </tr>
+                      <tr>
+                        <td class="bg-purple text-right">一對一約會特色：</td>
+                        <td class="bg-pink">限定版-7分鐘Speed Dating 1對1聯誼，完全屬於您的約會，不讓別人專美於前！
+不管你害羞、不善表達，可獨自與對方交談的好機會，是你最讚的選擇！
+本場次將年紀縮小範圍，讓您更易找到相同話題對象！
+
+＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+√ 擁有絕對的聊天空間，展現個人特色
+√ 不需擔心話題被打斷或直接與他人比較
+√ 容易記得對方，進而提高成功配對機率
+
+￭ 11:30→報到、資料驗證
+￭ 12:00→主持人分享交友成功心法
+￭ 12:10→一對一約會時間
+￭ 14:00→ 自由交流時間
+￭ 14:10→一對一約會時間
+￭ 15:30→珍重再見
+</td>
+                      </tr>
+                      <tr>
+                        <td class="bg-purple text-right">提醒事項：</td>
+                        <td class="bg-blue">一、請詳閱每場<其他注意事項>，以維護您的權益及當日需帶之資料、本站規定
+二、機會是留給準備好的人，事先擬定話題交友更順利
+三、現場提供炒飯、炒麵、現炒熱食、沙拉吧、冰熱飲品、當季新鮮水果..等餐飲
+四、初次見面第一印象最重要，正式禮儀社交；男仕請著(有領)襯杉、POLO杉，         女仕請著裙裝或包鞋，乾淨、雅觀。＊嚴禁短褲、露趾涼鞋＊
+五、已參加過10/27(日)台北Speed dating 的單身同學們，本場將列入「備取」       哦！
+六、報名後須3日內繳費，超過時間恕無法保留，倒數名額採匯款優先正取。
+</td>
+                      </tr>
+                    </table>
+                                        
+				  <div class="dashline" style="margin:10px 0;" ></div>
+                    
+                    
+                  <div id="group_info">
+                    <div class="header">
+                        <h2><img src="images/icon-logo-purple.png" width="18" height="16">詳細說明</h2>
+                    </div>
+                    <div id="index_information">
+                        <div class="index_tab_select clearfix" class="navcontainer">
+                            <ul class="tabs">
+                                <li id="info-01"><a href="#">活動費用與參加資格</a></li>
+                                <li id="info-02"><a href="#">聯誼活動付款方式</a></li>
+                                <li id="info-03"><a href="#">特別優惠</a></li>
+                                <li id="info-04"><a href="#">其他注意事項</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                        <!-- tab "panes" -->
+                        <div class="panes p-info">
+                            <!-- 活動費用與參加資格 tab -->
+                            <div class="clearfix">
+                                <p>
+                                  
+  活動費用與參加資格
+      男士費用：1100元；活動5日前匯款，優惠價950元
+      女士費用：550元；活動5日前匯款，優惠價450元
+      男參加基本資格：學歷 大學以上，年齡介於64~73年次
+      女參加基本資格：學歷 專科以上，年齡介於67~76年次
+      報名後須於3日內繳費，超過時間不保留名額(依匯款之先後順序決定正取、備取人員)。
+  
+  </p>
+                            
+                            </div>
+                            <!--end 活動費用與參加資格 tab -->
+                            
+                            <!-- 聯誼活動付款方式 tab -->
+                            <div class="clearfix">
+                                <p>
+                                  
+  聯誼活動付款方式 
+      男士費用：1100元；活動5日前匯款，優惠價950元
+      女士費用：550元；活動5日前匯款，優惠價450元
+      男參加基本資格：學歷 大學以上，年齡介於64~73年次
+      女參加基本資格：學歷 專科以上，年齡介於67~76年次
+      報名後須於3日內繳費，超過時間不保留名額(依匯款之先後順序決定正取、備取人員)。
+  
+  </p>
+                            
+                            </div>
+                            <!--end 聯誼活動付款方式 tab -->
+                            
+                            <!-- 特別優惠 tab -->
+                            <div class="clearfix">
+                                <p>
+                                  
+  特別優惠
+      男士費用：1100元；活動5日前匯款，優惠價950元
+      女士費用：550元；活動5日前匯款，優惠價450元
+      男參加基本資格：學歷 大學以上，年齡介於64~73年次
+      女參加基本資格：學歷 專科以上，年齡介於67~76年次
+      報名後須於3日內繳費，超過時間不保留名額(依匯款之先後順序決定正取、備取人員)。
+  
+  </p>
+                            
+                            </div>
+                            <!--end 特別優惠 tab -->
+                            
+                            <!-- 其他注意事項 tab -->
+                            <div class="clearfix">
+                                <p>
+                                  其他注意事項
+  
+      男士費用：1100元；活動5日前匯款，優惠價950元
+      女士費用：550元；活動5日前匯款，優惠價450元
+      男參加基本資格：學歷 大學以上，年齡介於64~73年次
+      女參加基本資格：學歷 專科以上，年齡介於67~76年次
+      報名後須於3日內繳費，超過時間不保留名額(依匯款之先後順序決定正取、備取人員)。
+  
+  </p>
+                            
+                            </div>
+                            <!--end 其他注意事項 tab -->
+                            
+                        </div>
+						<!-- end panes-->
+                        </div>
+                    </div>
+                    <!-- end information-->
+                </div>
+                <!-- end 精選會員 -->
+                
+                <div align="center">
+                  <select name="" style="margin-right:10px;">
+                    <option>請選擇您要看的其他活動詳情</option>
+                  </select>
+                  
+                  <button class="btn-purple">活動列表</button>
+                  <button class="btn-purple">我要報名</button>
+                </div>
+                    
+                	
+          </div><!-- end page content-->
+                
+                            
+            </div><!-- end 右邊欄位 right col-->
+        </div>
+    	
+    </section>
+    
+    <!-- include footer-->
+    <?php include("include_footer.php");?>
+    <!-- end footer-->
+    
+    <!-- include scripts-->
+    <?php include("include_scripts.php");?>
+    <!-- end scripts-->
+
+
+
+
+</body>
+</html>
