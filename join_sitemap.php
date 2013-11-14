eeb195effa111bcb9183b769a1fb4a828145c79d
Hsiao <chiyen29@gmail.com>
2013-11-15 02:04:47 +0800

    add new pages
    認識本站/新手加入/

---
+++ join_sitemap.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+    <title>戀戀未來</title>
+
+    
+    <link href="css/normalize.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/layout.css" rel="stylesheet">
+    <link href="css/tabs.css" rel="stylesheet">
+
+    <!-- HTML5 shim and IE8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="/js/html5shiv.js"></script>
+    <![endif]-->
+    
+   
+  </head>
+
+<body>
+    
+<!-- include header -->	
+ <?php include("include_header.php");?>
+ 
+	
+    <section class="wrapper clearfix">
+    	
+        <div class="since"><img src="images/since.png"></div>
+        
+        <!-- 右方搜尋-->
+		 <?php include("include_right_search.php");?>
+        <!-- end 右方搜尋-->
+        
+        <div id="content" class="clearfix">
+        
+        	<div class="banner" style="width:940px; height:300px;">
+       	    	<img src="images/banner.jpg"> 
+            </div>
+        	
+            
+            <!-- 左邊欄位-->
+            <div id="left-col">
+                
+                <!-- 左欄內容-->
+               	<?php include("include_left-col.php");?>
+                <!-- end 左欄內容-->
+                
+          </div><!-- end  左邊欄位-->
+            
+            <div id="right-col">
+            	<div class="text-slider">
+           	    	<img src="images/icon-logo-purple.png" width="18" height="16"> 
+                    <span class="font-red">10/05  新竹地區 戶外聯誼   </span>
+                    <span class="font-blue">☆ 企業聯誼☆  戀戀香草幸福出遊-戶外之旅，目前男性報名資料.   >小帆25 歲 / 桃園縣 </span>  
+                </div>
+                <div class="about_square">
+                        <h2 class="heading-purple">
+                        網站導覽
+                        </h2>
+                    <div class="heading-dashline"></div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 認識本站｜</div>
+                      <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">入會流程</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">線上排約流程</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">報名繳費</a></li>
+                        </ul>
+                    </div>
+                  <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 相親排約｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">線上交友</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">客制相親</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">相親問答</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">服務比較</a></li>
+                        </ul>
+                    </div>
+                  <div class="top-selection2">
+                    <div class="selection-category_1"></div>
+                    <div class="selection-category_1"></div>
+                       <ul class="clearfix">
+                          <li><a href="#">&nbsp;&nbsp;&nbsp;&nbsp;<img src="./images/e-more-pink.gif">相親指南</a></li>
+                           <li></li>
+                           <div class="selection-category_2"></div>
+                           
+                           <li><a href="#"><img src="./images/e-more-pink.gif">各區排約聚點</a></li>
+                       </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 團體聯誼｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">區域聯誼活動列表</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">活動說明</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 新手加入｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">服務項目</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">加入流程</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">網站導覽</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 戀戀園地｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">聯誼花絮</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">甜蜜分享</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">戀戀論壇</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">戀戀好康</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 聯絡我們｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">聯絡方式</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">常見問題</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">留言板</a></li>
+                        </ul>
+                        <div class="heading-dashline_1"></div>
+                    </div>
+                  <div class="top-selection1">
+                    <div class="selection-category_1">&gt;&gt; 活動公告｜</div>
+                    <ul class="clearfix">
+                          <li><a href="#"><img src="./images/e-more-pink.gif">活動公告</a></li>
+                     </ul>
+                    </div>
+                  <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 會員專區｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">個人基本資料</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">活動紀錄 / 繳費回報</a></li>
+                            <li><a href="#"><img src="./images/e-more-pink.gif">報名聯誼活動</a></li>
+                        </ul>
+                    </div>
+                   <div class="top-selection1">
+                        <div class="selection-category_1"></div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">報名排約 / 修改排約資料</a></li>
+                        </ul>
+                        <div class="heading-dashline_1"></div>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 會員條款｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">會員條款</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 合作提案｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">合作提案</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 友情連結｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">友情連結</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 親友推薦｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">親友推薦</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 網站導覽｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">網站導覽</a></li>
+                        </ul>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 本站徵才｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">本站徵才</a></li>
+                        </ul>
+                        <div class="heading-dashline_1"></div>
+                    </div>
+                    <div class="top-selection1">
+                        <div class="selection-category_1">&gt;&gt; 進階搜尋｜</div>
+                        <ul class="clearfix">
+                            <li><a href="#"><img src="./images/e-more-pink.gif">進階搜尋</a></li>
+                        </ul>
+                    </div>
+                            
+            </div><!-- end 右邊欄位 right col-->
+        </div>
+    	
+    </section>
+    
+    <!-- include footer-->
+    <?php include("include_footer.php");?>
+    <!-- end footer-->
+    
+    <!-- include scripts-->
+    <?php include("include_scripts.php");?>
+    <!-- end scripts-->
+
+
+
+
+</body>
+</html>
